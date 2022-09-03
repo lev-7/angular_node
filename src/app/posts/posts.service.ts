d961139bd3bb68031306fb3f4fb140e16feb785a
Yasin Cakar <s2921450@student.ad.griffith.edu.au>
2022-09-03 22:33:51 +1000

    42. Adding Angular

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -26,9 +26,12 @@ export class PostService{
 
   addPost(title:string,content:string){
     const post: Post = { id: null, title: title, content: content };
-    this.posts.push(post);
+    this.http.post<{message:string}>("http://localhost:3000/api/posts",post).subscribe(responseData =>{
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts ]);
+    })
     console.log("SERVICE--PostService--addPost");
     console.log(this.posts);
-    this.postsUpdated.next([...this.posts ]);
   }
 }
